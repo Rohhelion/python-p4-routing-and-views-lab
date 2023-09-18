cab55cf5540c4a250592ed2d8e580ba6830b5a4e
Rohhelion <kirorei04@gmail.com>
2023-09-18 17:48:34 +0300

    completed lab

--- server/app.py
+++ server/app.py
@@ -1,8 +1,40 @@
-#!/usr/bin/env python3
-
 from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<string:text>')
+def print_string(text):
+    print(f'Printed: {text}')
+    return f'<p>Printed: {text}</p>'
+
+@app.route('/count/<int:num>')
+def count(num):
+    numbers = '<pre>' + '\n'.join(map(str, range(num))) + '</pre>'
+    return numbers
+
+@app.route('/math/<float:num1><operation><float:num2>')
+def math(num1, operation, num2):
+    if operation == '+':
+        result = num1 + num2
+    elif operation == '-':
+        result = num1 - num2
+    elif operation == '*':
+        result = num1 * num2
+    elif operation == 'div':
+        if num2 != 0:
+            result = num1 / num2
+        else:
+            return "Division by zero is not allowed"
+    elif operation == '%':
+        result = num1 % num2
+    else:
+        return "Invalid operation"
+    
+    return f'<p>{num1} {operation} {num2} = {result}</p>'
+
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+    app.run(port=5555)
